1c37b079346b4793e8cc54ab0842ed38b3b8f3b8
EC2 Default User <ec2-user@ip-172-31-41-253.us-east-2.compute.internal>
2020-03-22 20:40:59 +0000

    bug fixed and test passed

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -28,8 +28,6 @@ module NavigationHelpers
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
     
-    when /^the new categories page$/
-      '/admin/categories/new'
 
     else
       begin
